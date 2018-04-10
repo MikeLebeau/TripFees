6f5284b86b70b9dca66a1655d99af24a706ef72d
Dan <magier.dan.pro@gmail.com>
2018-04-10 15:59:42 +0200

    Project with holes to fill in the code

--- infrastructure/src/test/java/com/magier/tripservice/infrastructure/ti/TravelFeesRepoTest.java
+++ infrastructure/src/test/java/com/magier/tripservice/infrastructure/ti/TravelFeesRepoTest.java
@@ -8,6 +8,7 @@ import com.magier.tripservice.infrastructure.liquibase.LiquibaseHelper;
 import liquibase.Liquibase;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,7 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
+@Ignore
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {AppConfig.class})
 public class TravelFeesRepoTest {
@@ -46,13 +47,6 @@ public class TravelFeesRepoTest {
 
     @Test
     public void test_findTripByDestination_with_a_valid_destination_should_find_a_trip() {
-        Destination paris = new Destination();
-        paris.setName("Paris");
 
-        Trip parisTrip = tripRepositoryPort.findTripByDestination(paris);
-
-        Trip expectedTrip = new Trip(paris, 300, 50);
-        assertThat(parisTrip.agencyFees()).isEqualTo(expectedTrip.agencyFees());
-        assertThat(parisTrip.travelFees()).isEqualTo(expectedTrip.travelFees());
     }
 }
